5417b70ea202f2869a60b7f4d593fe793d5df95f
dfmr96 <dfmr96@gmail.com>
2022-09-11 20:27:03 -0300

    Velocidad inicial aleatoria

--- New Unity Project/Assets/Scripts/Setup.cs
+++ New Unity Project/Assets/Scripts/Setup.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 public class Setup : MonoBehaviour
 {
     Rigidbody rb;
-    float speed = 5;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.velocity = new Vector3(0, 0, speed);
+        rb.velocity = new Vector3(Random.Range(-5,5), Random.Range(5,8), Random.Range(8,12));
     }
 }
